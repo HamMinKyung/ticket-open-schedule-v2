49a0d60d40708d2651a0c0480b7776e6421112a6
minkyungHam <mk.ham@innods.com>
2025-05-20 11:17:28 +0900

    스케쥴링 매일, 당일 + 7일

--- run.py
+++ run.py
@@ -8,20 +8,24 @@ from crawler.melon import MelonCrawler
 from crawler.sejongpac import SejongPac
 from merge.merge import merge_ticket_sources
 from notion_writer.writer import NotionRepository
-
+from datetime import datetime, timedelta
 
 def calc_date_range() -> Tuple[datetime, datetime]:
-    from datetime import datetime, timedelta
 
     today = datetime.now()
 
     # 기준: 차주 월요일 00:00
-    days_until_next_monday = (7-today.weekday()) % 7 + 0
-    # days_until_next_monday = (today.weekday()) % 7 + 0
-    start = (today + timedelta(days=days_until_next_monday)).replace(hour=0, minute=0, second=0, microsecond=0)
+    # 실행일이 일요일이므로, 차주 월요일은 오늘 + 1일
+    #start = (today + timedelta(days=1)).replace(hour=0, minute=0, second=0, microsecond=0)
 
     # 금요일 23:59
-    end = start + timedelta(days=4, hours=23, minutes=59)
+    #end = (start + timedelta(days=4)).replace(hour=23, minute=59, second=0, microsecond=0)
+
+    # 당일 00:00
+    start = today.replace(hour=0, minute=0, second=0, microsecond=0)
+
+    # 7일 뒤 23:59
+    end = (start + timedelta(days=7)).replace(hour=23, minute=59, second=0, microsecond=0)
 
     print(f"실행 일자 {start} - {end}")
     return start, end
@@ -35,6 +39,7 @@ async def main():
 
     all_tickets = [t for sub in results for t in sub]
     merged = merge_ticket_sources(all_tickets)
+    print("Merged tickets:", len(merged))
 
     repo = NotionRepository()
     await repo.write_all(merged)
